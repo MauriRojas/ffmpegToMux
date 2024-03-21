5daafe0593576f8ba4e0cb82f76b57286cb0a53e
Mauricio Rojas <mauricio.rojas@southworks.com>
2024-03-21 17:54:17 -0300

    Added file to code

--- VirtualEncoder/index.js
+++ VirtualEncoder/index.js
@@ -1,14 +1,19 @@
 const childProcess = require('child_process');
 const { resolve } = require('path');
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const path = require("path");
 
 const streamToMux = (context) => {
     return new Promise(async (resolve, reject) => {
+        context.log(`Running ffmpeg from ${ffmpegPath}`);
+
+        var inputAbsolutePath = path.resolve("./input.mp4");
+
         // ffmpeg -re -i myfile_1.mp4 -r 30 -c:v libx264 -x264-params keyint=60:scenecut=0 -preset fast -b:v 5M -maxrate 6M -bufsize 3M -threads 4 -f flv rtmp://global-live.mux.com:5222/app/{my_stream_key}
         const child = childProcess.spawn(
             ffmpegPath,
             // note, args must be an array when using spawn
-            ['-re', '-i', 'https://jobssa.blob.core.windows.net/mp4/input.mp4?sp=r&st=2024-03-21T20:21:27Z&se=2024-03-22T04:21:27Z&spr=https&sv=2022-11-02&sr=b&sig=nBTRh9KCyozHqseJ408CHA7FbJlDeVzb3ljQiX%2Bwa24%3D', '-r', '30', '-c:v', 'libx264', '-x264-params', 'keyint=60:scenecut=0', '-preset', 'fast', '-b:v', '5M', '-maxrate', '6M', '-bufsize', '3M', '-threads', '4', '-f', 'flv', 'rtmp://global-live.mux.com:5222/app/5ac28812-8320-0c76-2ba2-313288af035f'],
+            ['-re', '-i', `${inputAbsolutePath}`, '-r', '30', '-c:v', 'libx264', '-x264-params', 'keyint=60:scenecut=0', '-preset', 'fast', '-b:v', '5M', '-maxrate', '6M', '-bufsize', '3M', '-threads', '4', '-f', 'flv', 'rtmp://global-live.mux.com:5222/app/5ac28812-8320-0c76-2ba2-313288af035f'],
             {
                 windowsVerbatimArguments: true,
             }
