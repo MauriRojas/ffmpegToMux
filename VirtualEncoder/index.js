0156e63b26ce47c5a106206b49ce2bab778540e1
Mauricio Rojas <mauricio.rojas@southworks.com>
2024-03-21 17:25:28 -0300

    Wraps ffmpeg in a promise

--- VirtualEncoder/index.js
+++ VirtualEncoder/index.js
@@ -1,5 +1,46 @@
 const childProcess = require('child_process');
+const { resolve } = require('path');
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+
+const streamToMux = (context) => {
+    return new Promise(async (resolve, reject) => {
+        // ffmpeg -re -i myfile_1.mp4 -r 30 -c:v libx264 -x264-params keyint=60:scenecut=0 -preset fast -b:v 5M -maxrate 6M -bufsize 3M -threads 4 -f flv rtmp://global-live.mux.com:5222/app/{my_stream_key}
+        const child = childProcess.spawn(
+            ffmpegPath,
+            // note, args must be an array when using spawn
+            ['-re', '-i', 'https://jobssa.blob.core.windows.net/mp4/input.mp4?sp=r&st=2024-03-21T20:21:27Z&se=2024-03-22T04:21:27Z&spr=https&sv=2022-11-02&sr=b&sig=nBTRh9KCyozHqseJ408CHA7FbJlDeVzb3ljQiX%2Bwa24%3D', '-r', '30', '-c:v', 'libx264', '-x264-params', 'keyint=60:scenecut=0', '-preset', 'fast', '-b:v', '5M', '-maxrate', '6M', '-bufsize', '3M', '-threads', '4', '-f', 'flv', 'rtmp://global-live.mux.com:5222/app/5ac28812-8320-0c76-2ba2-313288af035f'],
+            {
+                windowsVerbatimArguments: true,
+            }
+        );
+
+        child.stdout.on('data', (data) => {
+            context.log(`stdout: ${data}`);
+        });
+
+        child.stderr.on('data', (data) => {
+            context.log(`stderr: ${data}`);
+        });
+
+        child.on('error', (error) => {
+            // catches execution error (bad file)
+            context.log(`Error executing binary: ${ffmpegPath}`);
+            reject(error);
+        });
+
+        child.on('close', (code) => {
+            context.log(`Process exited with code: ${code}`);
+            if (code === 0) {
+                context.log("Finished gracefully");
+                resolve('Streamed successfully')
+            } else {
+                context.log(`FFmpeg encountered an error, check the console output`);
+                console.log(child);
+                reject(`FFmpeg encountered an error, check the console output`);
+            }
+        });
+    });
+};
 
 module.exports = async function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processing work item', myQueueItem);
@@ -7,38 +48,7 @@ module.exports = async function (context, myQueueItem) {
     // 2. Process the message here
     context.log('Starting streaming blob');
 
-    // ffmpeg -re -i myfile_1.mp4 -r 30 -c:v libx264 -x264-params keyint=60:scenecut=0 -preset fast -b:v 5M -maxrate 6M -bufsize 3M -threads 4 -f flv rtmp://global-live.mux.com:5222/app/{my_stream_key}
-    const child = childProcess.spawn(
-        ffmpegPath,
-        // note, args must be an array when using spawn
-        ['-re', '-i', 'https://jobssa.blob.core.windows.net/mp4?sp=r&st=2024-03-21T16:40:56Z&se=2024-03-22T00:40:56Z&spr=https&sv=2022-11-02&sr=c&sig=Avyr6lhXlpBdH6iK3X5hlRyrJ%2FJx0nPxkPeBYsFbAZg%3D', '-r', '30', '-c:v', 'libx264', '-x264-params', 'keyint=60:scenecut=0', '-preset', 'fast', '-b:v', '5M', '-maxrate', '6M', '-bufsize', '3M', '-threads', '4', '-f', 'flv', 'rtmp://global-live.mux.com:5222/app/5ac28812-8320-0c76-2ba2-313288af035f'],
-        {
-            windowsVerbatimArguments: true,
-        }
-    );
+    var finished = await streamToMux(context);
 
-    child.stdout.on('data', (data) => {
-        context.log(`stdout: ${data}`);
-    });
-
-    child.stderr.on('data', (data) => {
-        context.log(`stderr: ${data}`);
-    });
-
-    child.on('error', () => {
-        // catches execution error (bad file)
-        context.log(`Error executing binary: ${ffmpegPath}`);
-        throw "error while running ffmpeg";
-    });
-
-    child.on('close', (code) => {
-        context.log(`Process exited with code: ${code}`);
-        if (code === 0) {
-            resolve(outputDir);
-        } else {
-            context.log(`FFmpeg encountered an error, check the console output`);
-        }
-    });
-
-    context.log("Message processed, testing image from local environment");
+    context.log("Finished execution", finished);
 };
