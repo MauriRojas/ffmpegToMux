01e73f2eb01d3a9f09dc9eaf964c215c042bf406
Mauricio Rojas <mauricio.rojas@southworks.com>
2024-03-22 08:39:55 -0300

    Removes additional ffmpeg parameters

--- VirtualEncoder/index.js
+++ VirtualEncoder/index.js
@@ -13,7 +13,7 @@ const streamToMux = (context) => {
         const child = childProcess.spawn(
             ffmpegPath,
             // note, args must be an array when using spawn
-            ['-re', '-i', `${inputAbsolutePath}`, '-r', '30', '-c:v', 'libx264', '-x264-params', 'keyint=60:scenecut=0', '-preset', 'fast', '-b:v', '5M', '-maxrate', '6M', '-bufsize', '3M', '-threads', '4', '-f', 'flv', 'rtmp://global-live.mux.com:5222/app/5ac28812-8320-0c76-2ba2-313288af035f'],
+            ['-i', `${inputAbsolutePath}`, '-f', 'flv', 'rtmp://global-live.mux.com:5222/app/5ac28812-8320-0c76-2ba2-313288af035f'],
             {
                 windowsVerbatimArguments: true,
             }
